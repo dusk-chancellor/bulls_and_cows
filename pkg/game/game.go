310a5e6ea05fe146fa352b0d767439395276f8a5
dior <diorbenskhodji@gmail.com>
2023-12-27 15:15:47 +0500

    random number generator fixed tests fixed

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -2,20 +2,17 @@ package game
 
 import (
 	"math/rand"
+	"slices"
 )
 
-func remove(s []int, i int) []int {
-	s[i] = s[len(s)-1]
-	return s[:len(s)-1]
-}
-
-func RandomNumberGeneration() (num int) {
-	digits := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	l := 9
-	for i := 1; i <= 1000; i *= 10 {
-		pick := digits[rand.Intn(l)]
-		num += pick * i
-		remove(digits, pick)
+func RandomNumberGeneration() (num string) {
+	digits := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	l := 10
+	for i := 0; i <= 3; i++ {
+		index := rand.Intn(l)
+		n := digits[index]
+		num += n
+		slices.Delete(digits, index, index+1)
 		l--
 	}
 	return
--- tests/rand_gen_test.go
+++ tests/rand_gen_test.go
@@ -2,13 +2,16 @@ package tests
 
 import (
 	"bulls_and_cows/pkg/game"
-	"strconv"
 	"testing"
 )
 
 func TestRandomNumberGenerator(t *testing.T) {
-	thefunc := strconv.Itoa(game.RandomNumberGeneration())
-	if len(thefunc) != 4 {
-		t.Fatalf("The length of number must be 4, not %v", len(thefunc))
+	theFunc := game.RandomNumberGeneration()
+	if len(theFunc) != 4 {
+		t.Fatalf("The length of number must be 4, not %v", len(theFunc))
+	}
+	if theFunc[0] == theFunc[1] || theFunc[0] == theFunc[2] || theFunc[0] == theFunc[3] ||
+		theFunc[1] == theFunc[2] || theFunc[1] == theFunc[3] || theFunc[2] == theFunc[3] {
+		t.Fatalf("Digits within the number are repeated")
 	}
 }
